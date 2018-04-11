c00ed0b4665099e0dd894c2ed8df4fc400158424
giovanniguzzardi <giovanniguzzardi@192.168.1.134>
2018-04-11 22:10:43 +0200

    # WARNING: head commit changed in the meantime
    csv

--- F_data/src/main/java/it/frankenstein/data/service/CallingApiService.java
+++ F_data/src/main/java/it/frankenstein/data/service/CallingApiService.java
@@ -92,8 +92,51 @@ public class CallingApiService {
 		String price = getPrice(symbol);
 		ObjectMapper ob = new ObjectMapper();
 		Map<String, String> map = ob.readValue(price, Map.class);
-		dataHandler.handleDispose(symbol, map.get("price"));;
+		dataHandler.handleDispose(symbol, map.get("price"));
 		return "ok";
 	}
+	
+	public String order(String symbol, double amount) throws InterruptedException, ExecutionException, JsonParseException, JsonMappingException, IOException {
+		String path = "/api/v3/order/test";
+		String url = commonConfig.getUrl();
+		MultivaluedMap<String, String> params = getQueryParams(symbol, amount);
+		String response = "";
+		try {
+			response = c.asyncResource(url)
+					.path(path)
+					.queryParams(params)
+					.header("X-MBX-APIKEY", "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A")
+					.accept(MediaType.APPLICATION_JSON)
+					.post(String.class).get();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		
+		ObjectMapper ob = new ObjectMapper();
+		Map<String, String> map = ob.readValue(response, Map.class);
+		return map.toString();
+	}
+	
+	public MultivaluedMap<String, String> getQueryParams(String symbol, double amount) {
+		MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+		params.add("symbol", symbol);
+		params.add("side", "BUY"); //BUY or SELL
+		params.add("type", "LIMIT"); // LIMIT or MARKET or STOP_LOSS or STOP_LOSS_LIMIT or TAKE_PROFIT or TAKE_PROFIT_LIMIT or LIMIT_MAKER
+		//params.add("timeInForce", ""); //no mandatory ENUM NO	
+		params.add("quantity", Double.toString(amount));
+		//params.add("price", ""); // DECIMAL	NO	
+		//params.add("newClientOrderId", ""); //	STRING	NO	A unique id for the order. Automatically generated if not sent.
+		//params.add("stopPrice", ""); //	DECIMAL	NO	Used with STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, and TAKE_PROFIT_LIMIT orders.
+		//params.add("icebergQty", ""); //	DECIMAL	NO	Used with LIMIT, STOP_LOSS_LIMIT, and TAKE_PROFIT_LIMIT to create an iceberg order.
+		params.add("newOrderRespType", "FULL"); //	ENUM	NO	Set the response JSON. ACK, RESULT, or FULL; default: RESULT.
+		//params.add("recvWindow", ""); //LONG	NO	
+		params.add("timestamp", Long.toString(System.currentTimeMillis())); //LONG	YES
+		
+		params.add("signature", "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j");
+		
+		return params;
+	}
+	
 
 }
